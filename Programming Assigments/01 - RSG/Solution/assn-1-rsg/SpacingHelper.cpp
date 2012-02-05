255954166d452d4d918fb4efc72cde05687e3656
onedayitwillmake <mariogonzalez@gmail.com>
2012-02-05 10:56:19 -0500

    Code cleanup

--- Programming Assigments/01 - RSG/Solution/assn-1-rsg/SpacingHelper.cpp
+++ Programming Assigments/01 - RSG/Solution/assn-1-rsg/SpacingHelper.cpp
@@ -10,8 +10,9 @@
 
 SpacingHelper::SpacingHelper() {
 	_rules["."] = "\n";
-	_rules[","] = "";
+	_rules[","] = " ";
 	_rules[""] = "";
+	_rules["s"] = "";
 }
 SpacingHelper::~SpacingHelper() {
 }
@@ -22,8 +23,10 @@ void SpacingHelper::getSpaceRuleFor( const std::string& needle, std::string&resu
 	std::map<std::string, std::string>::const_iterator found = _rules.find(needle);
 	if( found == _rules.end() ) {
 		result = " ";
+		std::cout << "Spacing rule NOT found for: '" << needle << "'" << std::endl;
 		return;
 	}
 
+	std::cout << "Spacing rule WAS found for: '" << needle << "'" << std::endl;
 	result = _rules[needle];
 }
